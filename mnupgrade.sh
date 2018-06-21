b4fe0df5c5d196a7b069601b4708f984bd4a7afd
a0li <40431902+a0li@users.noreply.github.com>
2018-06-21 04:26:43 +0100

    Update mnupgrade.sh

--- mnupgrade.sh
+++ mnupgrade.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
 TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='/root/.aqa/aqa.conf'
-MNCONFIG_FILE='/root/.aqa/masternode.conf'
-BKUP_FOLDER='/root/aqabkup'
-CONFIGFOLDER='/root/.aqa'
 COIN_DAEMON='aqad'
 COIN_CLI='aqa-cli'
-COIN_PATH='/usr/local/bin/'
 OLDCLI='/usr/local/bin/aqa-cli'
 OLDDAEMON='/usr/local/bin/aqad'
+COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/aquilacoin/Aquila/releases/download/1.1.0/Aqa-1.1-linux.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='aqa'
-
-
-NODEIP=$(curl -s4 icanhazip.com)
 
 
 RED='\033[0;31m'
@@ -24,7 +17,7 @@ NC='\033[0m'
 
 function download_node() {
   systemctl stop aqa 
-  echo -e "Preparing to download ${GREEN}$COIN_NAME${NC}."
+  echo -e "Preparing to upgrade ${GREEN}$COIN_NAME${NC}."
   cd $TMP_FOLDER >/dev/null 2>&1
   rm $COIN_ZIP >/dev/null 2>&1
   wget -q $COIN_TGZ
@@ -38,19 +31,6 @@ function download_node() {
   cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
-}
-
-function downloadconf() {
-  cd . 
-  mkdir aqabkup
-  cp $CONFIG_FILE $BKUP_FOLDER
-  cp $MNCONFIG_FILE $BKUP_FOLDER
-  cp -r '/root/.aqa/blocks' $BKUP_FOLDER
-  rm -r $CONFIGFOLDER
-  mkdir $CONFIGFOLDER
-  cp '/root/aqabkup/masternode.conf' $CONFIGFOLDER
-  cp '/root/aqabkup/aqa.conf' $CONFIGFOLDER
-  cp -r '/root/.aqa/blocks' $CONFIGFOLDER
 }
 
 function compile_error() {
@@ -76,5 +56,4 @@ function setup_node() {
 clear
 
 download_node
-downloadconf
 setup_node
